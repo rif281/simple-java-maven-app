d38df9f55e93a4d0015fd11a365c482c3c58cf4a
Reef Ashkenazi <102466625+rif281@users.noreply.github.com>
2024-01-07 15:38:35 +0200

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 resource "aws_instance" "for_simple_java_project" {
