94bc687260d79f0dc63c4d8be844ad7f5c757a5a
Reef Ashkenazi <102466625+rif281@users.noreply.github.com>
2024-01-07 16:59:17 +0200

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -15,6 +15,12 @@ resource "aws_instance" "for_simple_java_project" {
   ami                    = "ami-0fe87baa62d01cd60"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-0a488b17830c73da9"]
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker run rif281/simple_java"
+    ]
+  }
 
   tags = {
     Name = "for_simple_java_project"
